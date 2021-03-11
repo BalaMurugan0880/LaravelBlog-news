653ee0fafbbb9727e39f41f0c597157ba4e9a972
Balamurugan <balamuruganpgb08@gmail.com>
2021-03-11 12:15:14 +0800

    Added Reset Password Using Mail Function

--- config/mail.php
+++ config/mail.php
@@ -37,7 +37,7 @@ return [
         'smtp' => [
             'transport' => 'smtp',
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
+            'port' => env('MAIL_PORT', 25),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
@@ -84,10 +84,21 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'balamuruganpgb08@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'BalaMurugan'),
     ],
 
+/*
+|--------------------------------------------------------------------------
+| E-Mail Encryption Protocol
+|--------------------------------------------------------------------------
+|
+| Here you may specify the encryption protocol that should be used when
+| the application send e-mail messages. A sensible default using the
+| transport layer security protocol should provide great security.
+|
+*/
+'encryption' => env('MAIL_ENCRYPTION', 'tls'),
     /*
     |--------------------------------------------------------------------------
     | Markdown Mail Settings
