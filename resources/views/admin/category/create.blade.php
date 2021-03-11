d75662d234293f36ed0de266ce1b2eea93f57469
Balamurugan <balamuruganpgb08@gmail.com>
2021-03-11 09:46:14 +0800

    Added File Manager Post(Edit Page),Category(Create & Edit Page)

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -1,5 +1,12 @@
 @extends('admin.index')
 
+@section('stylesheet')
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+<link rel="stylesheet" href="{{ asset('vendor/file-manager/css/file-manager.css') }}">
+@endsection
+
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
@@ -18,11 +25,15 @@
                                 @endforeach
                             </select>
                         </div>
-                        <div class="form-group @if($errors->has('thumbnail')) has-error @endif">
-                            {!! Form::label('Thumbnail') !!}
-                            {!! Form::text('thumbnail', null, ['class' => 'form-control', 'placeholder' => 'Thumbnail']) !!}
-                            @if ($errors->has('thumbnail'))
-                                <span class="help-block">{!! $errors->first('thumbnail') !!}</span>@endif
+                        <div class="form-group">
+                            {!! Form::label('Featured Image') !!}
+                            <div class="input-group">
+                                <input type="text" id="image_label" class="form-control" name="thumbnail"
+                                       aria-label="Image" aria-describedby="button-image" placeholder="Select Or Upload A Image">
+                                <div class="input-group-append">
+                                    <button class="btn btn-outline-secondary" type="button" id="button-image">Select</button>
+                                </div>
+                            </div>
                         </div>
 
                         <div class="form-group @if($errors->has('name')) has-error @endif">
@@ -44,4 +55,29 @@
             </div>
         </div>
     </div>
+@endsection
+
+    @section('javascript')
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdn.ckeditor.com/4.16.0/full/ckeditor.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
+    <script src="{{ asset('vendor/file-manager/js/file-manager.js') }}"></script>
+
+
+
+<script type="text/javascript">
+    document.addEventListener("DOMContentLoaded", function() {
+
+  document.getElementById('button-image').addEventListener('click', (event) => {
+    event.preventDefault();
+
+    window.open('/file-manager/fm-button', 'fm', 'width=1400,height=800');
+  });
+});
+
+// set file link
+function fmSetLink($url) {
+  document.getElementById('image_label').value = $url;
+}
+</script>
 @endsection
--- resources/views/admin/post/edit.blade.php
+++ resources/views/admin/post/edit.blade.php
@@ -2,6 +2,9 @@
 
 @section('stylesheet')
     <link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet"/>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+<link rel="stylesheet" href="{{ asset('vendor/file-manager/css/file-manager.css') }}">
 @endsection
 
 @section('content')
@@ -13,11 +16,15 @@
 
                     <div class="card-body">
                         {!! Form::open(['route' => ['posts.update', $post->id], 'method' => 'put']) !!}
-                        <div class="form-group @if($errors->has('thumbnail')) has-error @endif">
-                            {!! Form::label('Thumbnail') !!}
-                            {!! Form::text('thumbnail', $post->thumbnail, ['class' => 'form-control', 'placeholder' => 'Thumbnail']) !!}
-                            @if ($errors->has('thumbnail'))
-                                <span class="help-block">{!! $errors->first('thumbnail') !!}</span>@endif
+                        <div class="form-group">
+                            {!! Form::label('Featured Image') !!}
+                            <div class="input-group">
+                                <input type="text" id="image_label" class="form-control" name="thumbnail"
+                                       aria-label="Image" aria-describedby="button-image" placeholder="Select Or Upload A Image" value="{{$post->thumbnail}}">
+                                <div class="input-group-append">
+                                    <button class="btn btn-outline-secondary" type="button" id="button-image">Select</button>
+                                </div>
+                            </div>
                         </div>
 
                         <div class="form-group @if($errors->has('title')) has-error @endif">
@@ -70,12 +77,12 @@
 @endsection
 
 @section('javascript')
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
-            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://cdn.ckeditor.com/4.16.0/full/ckeditor.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
+    <script src="{{ asset('vendor/file-manager/js/file-manager.js') }}"></script>
 
-    <script>
+         <script>
         $(document).ready(function () {
             CKEDITOR.replace('details');
 
@@ -86,8 +93,23 @@
         CKEDITOR.replace( 'details', {
             filebrowserUploadUrl: "{{route('upload', ['_token' => csrf_token() ])}}",
             filebrowserUploadMethod: 'form',
-            filebrowserBrowseUrl: '{{ asset('admin/post/file_browser') }}',
+             filebrowserImageBrowseUrl: '/file-manager/ckeditor',
              
         });
+            </script>
+            <script type="text/javascript">
+                document.addEventListener("DOMContentLoaded", function() {
+
+              document.getElementById('button-image').addEventListener('click', (event) => {
+                event.preventDefault();
+
+                window.open('/file-manager/fm-button', 'fm', 'width=1400,height=800');
+              });
+            });
+
+            // set file link
+            function fmSetLink($url) {
+              document.getElementById('image_label').value = $url;
+            }
             </script>
 @endsection
